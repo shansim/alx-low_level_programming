e7e904b52facd2e6907067a2b605b4c3d8669a42
shansim <sphinixsnikd@gmail.com>
2022-07-04 02:10:58 -0700

    Hot fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,16 @@
 
 int main(void)
 {
-	char a = 'a';
-	char b = 'A';
+char a = 'a';
+char b = 'A';
 
-	for (a = 'a'; a <= 'z'; a++)
-	{
-		putchar(a);
-	}
-	for (b = 'A'; b <= 'Z'; b++)
-	{
-		putchar(b);
-	}
-	putchar('\n');
-	return (0);
+for (a = 'a'; a <= 'z'; a++)
+{
+	putchar(a);
+}
+for (b = 'A'; b <= 'Z'; b++)
+{
+	putchar(b);
+}
+putchar('\n');
+return (0);
