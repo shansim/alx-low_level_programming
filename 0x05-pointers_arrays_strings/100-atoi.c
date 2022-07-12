18e9478504d06d4ad42c6f585810f1987ac6dd49
shansim <sphinixsnikd@gmail.com>
2022-07-12 04:00:34 +0300

    Hot fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,36 +11,20 @@
 
 int _atoi(char *s)
 {
-	unsigned int i = 0, z = 0, p = 0;
-	unsigned int h = 1, w = 1, n;
+	int sign = 1;
+	unsigned int num = 0;
 
-	while (s[i])
-	{
-		if (z > 0 && (s[i] < '0' || s[i] > '9'))
-		{
+	do {
+
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
 			break;
-		}
+	} while (*s++);
 
-		if (s[i] == '-')
-		{
-			h *= -1;
-		}
-
-		if ((s[i] >= '0') && (s[i] <= '9'))
-		{
-			if (z > 0)
-			{
-				w *= 10;
-			}
-			z++;
-		}
-		i++;
-	}
-
-	for (n = i - z; n < i; n++)
-	{
-		p = p + ((s[n] - 48) * w);
-		w /= 10;
-	}
-
-	return (p * h);
+	return (num * sign);
+}
