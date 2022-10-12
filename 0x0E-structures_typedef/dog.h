7738665ff29733dba0cdc560551d2262ab142d90
shansim <sphinixsnikd@gmail.com>
2022-10-12 15:03:49 -0700

    Task 0 commit on structs and typedef project

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,39 @@
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef HEADER_FILE
+
+#define HEADER_FILE dog.h
+
 /**
  *
- * * struct dog - Struct containing three members
+ * * struct dog - Define a new struct dog with name, age, owner
  *
- * * @name: First member
+ * * @name: name of the dog
  *
- * * @age: Second member
+ * * @age: age of the dog
  *
- * * @owner: Third member
+ * * @owner: owner of dog
  *
- * *
- *
- * * Description: Longer description
- *
- * */
+ * **/
+
 struct dog
+
 {
+
 	char *name;
+
 	float age;
+
 	char *owner;
-}
+
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
