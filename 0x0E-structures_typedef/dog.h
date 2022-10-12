affecc4b3ea0c276c3d4e6f6a25a9f45f69b4b6f
shansim <sphinixsnikd@gmail.com>
2022-10-12 14:59:43 -0700

    Task 0 commit on structs and typedef project

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *
+ * * struct dog - Struct containing three members
+ *
+ * * @name: First member
+ *
+ * * @age: Second member
+ *
+ * * @owner: Third member
+ *
+ * *
+ *
+ * * Description: Longer description
+ *
+ * */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
