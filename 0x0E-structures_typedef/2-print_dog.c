c262b9c2f5df315e561cabbbcf5604873567043a
shansim <sphinixsnikd@gmail.com>
2022-10-12 15:58:11 -0700

    New task commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,8 +1,10 @@
-#include "dog.h"
-
 #include <stdio.h>
 
+#include "dog.h"
+
 #include <stdlib.h>
+
+
 
 /**
  *
@@ -19,9 +21,11 @@ void print_dog(struct dog *d)
 {
 
 	if (d == NULL)
+
 	{
 
 		;
+
 	}
 
 	else
@@ -29,31 +33,52 @@ void print_dog(struct dog *d)
 	{
 
 		if (d->name != NULL)
+
 		{
 
 			printf("Name: %s\n", d->name);
+
 		}
+
 		else
-		{
-			printf("Name: (nil)\n");
-		}
-		if (d->age != NULL)
-		{
-			
-			printf("Age: %f\n", d->age);
-		}
-		else
-		{
-			printf("Age: %f\n", d->age);
-		}
-		if (d->owner != NULL)
-		{
-			printf("Owner: %s\n", d->owner);
-		}
-		else
+
 		{
 
-		}	printf("Owner: (nil)\n");
+			printf("Name: (nil)\n");
+
+		}
+
+		if (d->age != 0.0)
+
+		{
+
+			printf("Age: %f\n", d->age);
+
+		}
+
+		else
+
+		{
+
+			printf("Age: (nil)\n");
+
+		}
+
+		if (d->owner != NULL)
+
+		{
+
+			printf("Owner: %s\n", d->owner);
+
+		}
+
+		else
+
+		{
+
+			printf("Owner: (nil)\n");
+
+		}
 
 	}
 
