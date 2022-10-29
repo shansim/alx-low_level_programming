1c06eaaa973ed2bbd8e3ba99514946f18907079e
shansim <sphinixsnikd@gmail.com>
2022-10-29 05:16:40 -0700

    task fo freeing a list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - Frees a list.
+ * @head: Address of the first node of a list.
+ **/
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp1, *temp2;
+
+	temp1 = head;
+	while (temp1 != NULL)
+	{
+	temp2 = temp1->next;
+	free(temp1);
+	temp1 = temp2;
+	}
+}
