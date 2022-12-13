1cb8a9c23cff751e1f52a338f8bd6cb6da4f8d58
shansim <sphinixsnikd@gmail.com>
2022-12-13 14:50:56 -0800

    shared libraries

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Use pointer to change value in memory
+ * @s: pointer to int s location
+ * Return: Null/void
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * _strncpy -> a funcion to append some characters
+ * @dest: first param
+ * @src: second param
+ * @n: 3rd param
+ * Return: a string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
