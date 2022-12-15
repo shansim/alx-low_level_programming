2ad870055c3e575d387c19a7d22d6edd70e3345a
shansim <sphinixsnikd@gmail.com>
2022-12-14 23:50:15 -0800

    Hot fix for this task

---
+++ 0x18-dynamic_libraries/dynamic.c
@@ -0,0 +1,82 @@
+#include "main.h"
+int _putchar(char c)
+{
+    /* code for _putchar goes here */
+}
+int _islower(int c)
+{
+    /* code for _islower goes here */
+}
+int _isalpha(int c)
+{
+    /* code for _isalpha goes here */
+}
+int _abs(int n)
+{
+    /* code for _abs goes here */
+}
+int _isupper(int c)
+{
+    /* code for _isupper goes here */
+}
+int _isdigit(int c)
+{
+    /* code for _isdigit goes here */
+}
+int _strlen(char *s)
+{
+   /* code for _strlen goes here */
+}
+void _puts(char *s)
+{
+    /* code for _puts goes here */
+}
+char *_strcpy(char *dest, char *src)
+{
+    /* code for _strcpy goes here */
+}
+int _atoi(char *s)
+{
+    /* code for _atoi goes here */
+}
+char *_strcat(char *dest, char *src)
+{
+    /* code for _strcat goes here */
+}
+char *_strncat(char *dest, char *src, int n)
+{
+    /* code for _strncat goes here */
+}
+char *_strncpy(char *dest, char *src, int n)
+{
+    /* code for _strncpy goes here */
+}
+int _strcmp(char *s1, char *s2)
+{
+    /* code for _strcmp goes here */
+}
+char *_memset(char *s, char b, unsigned int n)
+{
+    /* code for _memset goes here */
+}
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    /* code for _memcpy goes here */
+}
+char *_strchr(char *s, char c)
+{
+    /* code for _strchr goes here */
+}
+unsigned int _strspn(char *s, char *accept)
+{
+    /* code for _strspn goes here */
+}
+char *_strpbrk(char *s, char *accept)
+{
+    /* code for _strpbrk goes here */
+}
+char *_strstr(char *haystack, char *needle)
+{
+
+    /* code for _strstr goes here */
+}
